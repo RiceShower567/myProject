72a17e5233389ea20eefcf0ffe6d132585f69626
RiceShower <1337546577@qq.com>
2025-10-04 16:28:02 +0800

    v1.3.1 取消了限时功能，新增了五局三胜制

--- finger_guessing.c
+++ finger_guessing.c
@@ -1,6 +1,6 @@
-#include <stdio.h>;
-#include <stdlib.h>;
-#include <time.h>;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define SHITOU 1
 #define JIANDAO 2
@@ -8,19 +8,30 @@
 
 int enemyPlay();
 int myPlay();
-int judge(int myChoice,int enemyChoice);
+void judge(int myChoice, int enemyChoice, int* myWin, int* enemyWin);
 
 int main() {
 
-    int flag = 0;   //记录输赢状态
+    int myWin = 0;  //记录玩家赢下的局数 
+    int enemyWin = 0;   //记录敌人赢下的局数 
     int game = 0;   //记录局数
 
-    while(flag == 0) {
+    srand(time(NULL));
+
+    while(myWin != 3 && enemyWin != 3 && game != 5) {
         game++;
         printf("\n第%d局开始\n请选择：\n1.石头\n2.剪刀\n3.布\n请输入：",game);
         int myChoice = myPlay();
         int enemyChoice = enemyPlay();
-        flag = judge(myChoice, enemyChoice);
+        judge(myChoice, enemyChoice, &myWin, &enemyWin);
+    }
+    
+    if(myWin > enemyWin) {
+        printf("\n比分为 %d : %d ,你赢了!\n",myWin,enemyWin);
+    } else if(myWin < enemyWin) {
+        printf("\n比分为 %d : %d ,你输了\n!",myWin,enemyWin);
+    } else if(myWin == enemyWin) {
+        printf("\n比分为 %d : %d ,平局。\n",myWin,enemyWin);
     }
     printf("\n游戏结束");
 
@@ -29,7 +40,6 @@ int main() {
 
 //对手出拳
 int enemyPlay() {
-    srand(time(NULL));
     return rand() % 3 + 1;
 
 }
@@ -52,7 +62,7 @@ int myPlay() {
 }    
 
 //判断输赢
-int judge(int myChoice,int enemyChoice) {
+void judge(int myChoice, int enemyChoice, int* myWin, int* enemyWin) {
 
     switch(myChoice) {
 
@@ -61,13 +71,15 @@ int judge(int myChoice,int enemyChoice) {
             switch(enemyChoice) {
                 case SHITOU:
                     printf("对面出石头，平局。\n");
-                    return 0;
+                    return;
                 case JIANDAO:
                     printf("对面出剪刀，胜利！\n");
-                    return 1;
+                    (*myWin)++;
+                    return;
                 case BU:
                     printf("对面出布，你输了！\n");
-                    return 0;
+                    (*enemyWin)++;
+                    return;
             }
 
         //玩家出剪刀
@@ -75,13 +87,15 @@ int judge(int myChoice,int enemyChoice) {
             switch(enemyChoice) {
                 case SHITOU:
                     printf("对面出石头，你输了！\n");
-                    return 0;
+                    (*enemyWin)++;
+                    return;
                 case JIANDAO:
                     printf("对面出剪刀，平局。\n");
-                    return 0;
+                    return;
                 case BU:
                     printf("对面出布，胜利！\n");
-                    return 1;
+                    (*myWin)++;
+                    return;
             }
 
         //玩家出布
@@ -89,13 +103,15 @@ int judge(int myChoice,int enemyChoice) {
             switch(enemyChoice) {
                 case SHITOU:
                     printf("对面出石头，胜利！\n");
-                    return 1;
+                    (*myWin)++;
+                    return;
                 case JIANDAO:
                     printf("对面出剪刀，你输了！\n");
-                    return 0;
+                    (*enemyWin)++;
+                    return;
                 case BU:
                     printf("对面出布，平局。\n");
-                    return 0;
+                    return;
             }
         
     }
